a6e0391568024ce7e44a7ea6dd6fb6a3d01fb8ce
Alan Lengkoan <alanlengkoan15@gmail.com>
2022-02-23 11:00:17 +0800

    update again

--- application/views/admin/base.php
+++ application/views/admin/base.php
@@ -24,14 +24,6 @@
     <?php empty($css) ? '' : $this->load->view($css); ?>
     <!-- end:: css local -->
 
-    <style>
-        .parsley-errors-list {
-            color: red;
-            list-style-type: none;
-            padding: 0;
-        }
-    </style>
-
     <script type="text/javascript" src="<?= assets_url() ?>admin/jquery/js/jquery.min.js"></script>
 </head>
 
@@ -269,60 +261,7 @@
     <script type="text/javascript" src="<?= assets_url() ?>admin/js/vertical/vertical-layout.min.js"></script>
     <script type="text/javascript" src="<?= assets_url() ?>admin/js/script.min.js"></script>
     <script type="text/javascript" src="<?= assets_url() ?>admin/sweetalert/js/sweetalert.min.js"></script>
-
-    <script type="text/javascript">
-        // untuk angka
-        function justAngka(e) {
-            if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190, 77]) !== -1 ||
-                (e.keyCode === 65 && (e.ctrlKey === true || e.metaKey === true)) ||
-                (e.keyCode >= 35 && e.keyCode <= 40)) {
-                return;
-            }
-            if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
-                e.preventDefault();
-            }
-        };
-
-        // untuk format harga
-        function autoSeparator(Num) {
-            Num += '';
-            Num = Num.replace('.', '');
-            Num = Num.replace('.', '');
-            Num = Num.replace('.', '');
-            Num = Num.replace('.', '');
-            Num = Num.replace('.', '');
-            Num = Num.replace('.', '');
-            x = Num.split('.');
-            x1 = x[0];
-            x2 = x.length > 1 ? '.' + x[1] : '';
-            var rgx = /(\d+)(\d{3})/;
-            while (rgx.test(x1))
-                x1 = x1.replace(rgx, '$1' + '.' + '$2');
-            return x1 + x2;
-        };
-
-        // untuk input nomor type text
-        (function($) {
-            $.fn.inputFilter = function(inputFilter) {
-                return this.on("input keydown keyup mousedown mouseup select contextmenu drop", function() {
-                    if (inputFilter(this.value)) {
-                        this.oldValue = this.value;
-                        this.oldSelectionStart = this.selectionStart;
-                        this.oldSelectionEnd = this.selectionEnd;
-                    } else if (this.hasOwnProperty("oldValue")) {
-                        this.value = this.oldValue;
-                        this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
-                    } else {
-                        this.value = "";
-                    }
-                });
-            };
-        }(jQuery));
-
-        $(".inputNumber").inputFilter(function(value) {
-            return /^-?\d*$/.test(value);
-        });
-    </script>
+    <script type="text/javascript" src="<?= assets_url() ?>admin/js/custom.js"></script>
 
     <!-- begin:: js local -->
     <?php empty($js) ? '' : $this->load->view($js); ?>
